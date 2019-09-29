577b522c8a2a00aad6aaca9eb17a6a0b8dd9f68e
ramikboxing <45014154+ramikboxing@users.noreply.github.com>
2019-09-29 21:42:45 +0300

    Add files via upload

---
+++ KPO4381_nmv/UnitTestProject/UnitTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Kpo4381_nmv.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        private CarNewLoad_lab4 newLoader;
+             
+        public UnitTest()
+        {
+            newLoader = new CarNewLoad_lab4("Lab4Cars.txt");
+        }
+        //Test 1
+        /*Проверка имени файла для загрузки*/
+        [TestMethod]
+        public void TestFileName()
+        {
+            var target = newLoader.getFileName;
+            Assert.AreEqual("Lab4Cars.txt", target);
+        }
+        //Тест 2
+        /*Проверка марки первой ммашины в получаемом списке*/
+        [TestMethod]
+        public void TestExecuteMethodCarModel()
+        {
+            newLoader.Execute();
+            var targetCarModel = newLoader.carsList[0].Mark;
+            Console.WriteLine("TestTest"+ newLoader.carsList[0].Mark);
+            Assert.AreEqual("Mersedes", targetCarModel);
+        }
+        //Тест 3
+        /*Проверка пустой список или нет*/
+        [TestMethod]
+        public void ChekListNotNull_File()
+        {
+            ICarFactory loader = new CarTestFactory(); // обращение к объектам через интер-с
+            ICarsListLoader Loader = loader.CreateCarListLoader();
+            Loader.Execute();// Заполнение списка
+            Assert.IsNotNull(Loader.carsList);// Проверка списка на пустату          
+        }
+
+    }
+}
+
