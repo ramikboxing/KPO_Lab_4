1f55c502052b3da37999ce24d22125ddf2eed47e
ramikboxing <45014154+ramikboxing@users.noreply.github.com>
2019-09-29 21:40:55 +0300

    Add files via upload

---
+++ KPO4381_nmv/Kpo4381_nmv.Lib/factory/CarSplitFileFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kpo4381_nmv.Lib
+{
+   public class CarSplitFileFactory : ICarFactory
+    {
+        public ICarsListLoader CreateCarListLoader()
+        {
+            return new CarsListSplitFileLoader(AppGlobalSetting.dataFileName);
+        }
+
+        public ICarListSaver CreateCarListSaver()
+        {
+            return null;
+        }
+    }
+}
